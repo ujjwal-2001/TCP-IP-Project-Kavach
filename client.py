ed20ef3dc688d7158fc9614e8e91e5b0a3104af3
ujjwal-2001 <149869490+ujjwal-2001@users.noreply.github.com>
2023-11-04 17:31:06 +0530

    auth_encrypt
    - Implemented public and private key concept

--- client.py
+++ client.py
@@ -1,49 +1,36 @@
 import socket
+from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import padding
-import ssl
-
+from cryptography.hazmat.primitives import hashes
 # Define server address and port
-server_address = ('127.0.0.1', 12345)
-
+server_address = ('10.114.240.86', 12345)
 # Create a socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Wrap the socket with SSL for secure communication
-ssl_client_socket = ssl.wrap_socket(client_socket, keyfile=None, certfile=None, server_side=False)
-
 # Connect to the server
-ssl_client_socket.connect(server_address)
-
+client_socket.connect(server_address)
 # Get user input for username and password
 username = input("Enter your username: ")
 password = input("Enter your password: ")
-
 # Send the username to the server
-ssl_client_socket.send(username.encode())
-
+client_socket.send(username.encode())
 # Receive the public key from the server
-public_key_pem = ssl_client_socket.recv(4096)
-
+public_key_pem = client_socket.recv(4096)
 # Load the server's public key
 server_public_key = serialization.load_pem_public_key(public_key_pem)
-
 # Encrypt the password with the server's public key
 encrypted_password = server_public_key.encrypt(
     password.encode(),
     padding.OAEP(
-        mgf=padding.MGF1(algorithm=padding.ALGORITHMS.SHA256),
-        algorithm=padding.ALGORITHMS.SHA256,
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
         label=None
     )
 )
-
 # Send the encrypted password to the server
-ssl_client_socket.send(encrypted_password)
-
+client_socket.send(encrypted_password)
 # Receive the authentication result
-response = ssl_client_socket.recv(1024).decode()
+response = client_socket.recv(1024).decode()
 print(response)
-
 # Close the connection
-ssl_client_socket.close()
+client_socket.close()
--- server.py
+++ server.py
@@ -1,24 +1,36 @@
 import socket
-from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
-import ssl
 
-# Generate RSA key pair for the server
+# Generate a server RSA key pair
 private_key = rsa.generate_private_key(
     public_exponent=65537,
     key_size=2048,
 )
 
-# Serialize the public key to send to the client
-public_key_pem = private_key.public_key().public_bytes(
+private_pem = private_key.private_bytes(
     encoding=serialization.Encoding.PEM,
-    format=serialization.PublicFormat.SubjectPublicKeyInfo,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=serialization.NoEncryption()
 )
 
+# Extract the public key for sharing with clients
+public_key = private_key.public_key()
+public_pem = public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
+
+# Dummy authentication data (replace with a database in a real application)
+auth_data = {
+    "user1": "password1",
+    "user2": "password2",
+}
+
 # Define server address and port
-server_address = ('127.0.0.1', 12345)
+server_address = ('10.114.240.86', 12345)
 
 # Create a socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -35,15 +47,15 @@ def authenticate(client_socket):
     # Receive the username
     username = client_socket.recv(1024).decode()
 
-    if username:
+    if username in auth_data:
         # Send the public key to the client
-        client_socket.send(public_key_pem)
+        client_socket.send(public_pem)
 
         # Receive the encrypted password from the client
         encrypted_password = client_socket.recv(4096)
 
-        # Decrypt the password using the private key
-        password = private_key.decrypt(
+        # Decrypt the password using the server's private key
+        decrypted_password = private_key.decrypt(
             encrypted_password,
             padding.OAEP(
                 mgf=padding.MGF1(algorithm=hashes.SHA256()),
@@ -52,19 +64,13 @@ def authenticate(client_socket):
             )
         ).decode()
 
-        # Dummy authentication data (replace with a database in a real application)
-        auth_data = {
-            "user1": "password1",
-            "user2": "password2",
-        }
-
         # Check if the received password matches the stored password
-        if auth_data.get(username) == password:
+        if auth_data[username] == decrypted_password:
             client_socket.send(b"Authentication successful.")
         else:
             client_socket.send(b"Authentication failed.")
     else:
-        client_socket.send(b"Username not provided.")
+        client_socket.send(b"User not found.")
 
     client_socket.close()
 
@@ -73,8 +79,5 @@ while True:
     client_socket, client_address = server_socket.accept()
     print(f"Connection from {client_address}")
 
-    # Wrap the socket with SSL for secure communication
-    ssl_client_socket = ssl.wrap_socket(client_socket, keyfile=None, certfile=None, server_side=True)
-
     # Authenticate the client
-    authenticate(ssl_client_socket)
+    authenticate(client_socket)
