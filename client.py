21deb529dccbe3a9f863b35c9efca88f5d1f42bc
ujjwal-2001 <149869490+ujjwal-2001@users.noreply.github.com>
2023-11-09 14:58:43 +0530

    simulation-data-0
    - Added Simulation Data
    - Client can send the data with 1 second gap

--- client.py
+++ client.py
@@ -5,18 +5,23 @@ from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
 import argparse
 import ipaddress
+import json
+import time
 
 # Command line interface
 parser = argparse.ArgumentParser(description="TCP Client. Usage: python client.py -sIP <server IPv4 address> (default: localhost) -p <port number> (defualt: 12345)")
 # Adding arguments/options
 parser.add_argument("-sIP", dest="server_ip", default='127.0.0.1', help="Server IPv4 address")
 parser.add_argument("-p", dest="port", default=12345, type=int, help="Port number")
+parser.add_argument("-sim", dest="simulation_number", default=0, type=int, help="Simulation number\n 0: Trains on different tracks\n 1: Trains moving towards each other on same track\n 2: Both trains moving in opposite direction on same track\n 3: Trains moving in same direction on same track ")
+parser.add_argument("-train", dest="trainID", default="A",choices=["A", "B"], help="Train ID\n A: train A, B: Train B")
 
 args = parser.parse_args()
 
 server_ip = args.server_ip  # Extracting server IP from Command line
 port = args.port    # Extracting port number from Command line
-
+sim = args.simulation_number
+trainID = args.trainID
 # Validate the provided server IP as a valid IPv4 address
 try:
     ipaddress.IPv4Address(server_ip)
@@ -63,5 +68,22 @@ client_socket.send(encrypted_password)
 # Receive the authentication result
 response = client_socket.recv(1024).decode()
 print(response)
-# Close the connection
-client_socket.close()
+
+# Loading Simulation Data
+data_file = open("simulation_data.json")
+sim_data = json.load(data_file)
+train_data= "trainB_data" if(trainID=="B") else "trainA_data"
+packets_data_to_send = sim_data["sim"+str(sim)][train_data]
+
+# Sending packets with 1 sec gap
+try:
+    for packet in packets_data_to_send:
+        # Send data to the server
+        packet_str = str(packet)
+        client_socket.sendall(packet_str.encode())
+        print(f"Sent: {packet}")
+        # Sleep for 1 second before sending the next data
+        time.sleep(1)
+
+finally:
+    client_socket.close()
